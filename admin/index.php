2fda0a2046381773bed5ba81abf115be946673b1
Mitchel "Father Pumpkin" Smith <msmit590@uncc.edu>
2021-04-17 20:12:53 -0400

    Update to modern working version
    Updated to include current SQL database as well as a short website

---
+++ admin/index.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+    <title>Madrid Libraries Project - Mitchel and James Smith</title>
+
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Menu Section Starts -->
+    <div class="menu">
+        <div class="wrapper">
+            Menu goes here!
+        </div>
+    </div>
+    <!-- Menu Section Starts -->
+
+    <!-- Main Section Starts -->
+    <div class="main-content">
+        <div class="wrapper">
+            Main content goes here!
+        </div>
+    </div>
+    <!-- Main Section Starts -->
+
+    <!-- Footer Section Starts -->
+    <div class="footer">
+        <div class="wrapper">
+            Footer goes here!
+        </div>
+    </div>
+    <!-- Footer Section Starts -->
+</body>
+
+</html>
