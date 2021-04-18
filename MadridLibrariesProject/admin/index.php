e0c35f32d15ba3da60d1300588c3dd0dd114772e
Mitchel "Father Pumpkin" Smith <msmit590@uncc.edu>
2021-04-17 22:08:34 -0400

    Hell Yeah the Authors works
    we done it

---
+++ MadridLibrariesProject/admin/index.php
@@ -0,0 +1,46 @@
+<html>
+
+<head>
+    <title>Madrid Libraries Project - Mitchel and James Smith</title>
+
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <?php include('./partials/menu.php'); ?>
+
+    <!-- Main Section Starts -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>DASHBOARD</h1>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </div>
+    <!-- Main Section ENDS -->
+
+    <?php include('./partials/footer.php'); ?>
+</body>
+
+</html>
---
+++ MadridLibrariesProject/admin/partials/menu.php
@@ -0,0 +1,23 @@
+<!-- Menu Section Starts -->
+<html>
+
+<head>
+    <title>Madrid Libraries Project - Mitchel and James Smith</title>
+
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="./index.php">Home</a></li>
+                <li><a href="./lists.php">Lists</a></li>
+                <li><a href="./books.php">Books</a></li>
+                <li><a href="./authors.php">Authors</a></li>
+                <li><a href="./illustrators.php">Illustrators</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Menu Section ENDS -->
