ff7fff9cfc15019d4a26ff1fb6376ff16f32acba
Mitchel "Father Pumpkin" Smith <mitchellornesmith@gmail.com>
2021-05-13 16:09:13 -0400

    Updated Lists Page
    Search and return number of books by year and grouped by age

--- MadridLibrariesProject/main/lists.php
+++ MadridLibrariesProject/main/lists.php
@@ -7,39 +7,45 @@
         <br /><br /><br />
         <table class="tbl-full">
             <tr>
-                <th>List ID</th>
-                <th>Ages</th>
                 <th>Year</th>
+                <th>num_books</th>
             </tr>
+
             <?php
 
             //set up the query
-            $sql = "SELECT * FROM lists";
+            $sql = "SELECT `year`, COUNT(*) AS num_books FROM lists INNER JOIN books ON books.list_id = lists.list_id  GROUP BY `year`";
 
             //execute the query
             $res = mysqli_query($conn, $sql);
             //count the rows
             $count = mysqli_num_rows($res);
-            
+
             if ($count > 0) {
                 //do things, not empty
                 while ($row = mysqli_fetch_assoc($res)) {
-                    $list_id = ucwords($row['list_id']);
-                    $ages = ucwords($row['ages']);
                     $year = ucwords($row['year']);
+                    $num_books = ucwords($row['num_books']);
 
             ?>
             <tr>
-                <td><?php echo $list_id ?></td>
-                <td><?php echo $ages ?></td>
-                <td><?php echo $year ?></td>
-                <td><a href="#" class="btn-secondary">More Info</a>
+                <form action="searchList.php" method="post">
+                    <td>
+                        <input type="checkbox" id=<?php echo "$year" ?> name=<?php echo "$year" ?>
+                            value=<?php echo "$year" ?>>
+                        <label for=<?php echo "$year" ?>> <?php echo "$year" ?> </label><br>
+                    </td>
+                    <td><?php echo $num_books ?></td>
+
+
             </tr>
             <?php
                 }
             } ?>
         </table>
-
+        <input type="submit" value="Submit">
+        <input type="reset">
+        </form>
     </div>
 </div>
 <!-- Main Section Starts -->
